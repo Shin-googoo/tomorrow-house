e62c8d180758199939702780753ed18d0aaff6dd
syg1702 <syg1702@gmail.com>
2024-03-14 15:42:07 +0900

    🚧 Implement deleting individual search-history-item

--- js/gnb-search.js
+++ js/gnb-search.js
@@ -7,10 +7,15 @@ const deleteAllButton = gnbSearchHistory.querySelector(
   '.search-history-header button'
 )
 
+const deletButtonList = gnbSearchHistoryList.querySelectorAll('.delete-button')
+
+function closeGnbSearchHistory() {
+  gnbSearchHistory.classList.remove('is-active')
+  window.removeEventListener('click', closeGnbSearchHistoryOnClickingOutside)
+}
+
 function closeGnbSearchHistoryOnClickingOutside(e) {
   if (!gnbSearch.contains(e.target)) {
-    gnbSearchHistory.classList.remove('is-active')
-    window.removeEventListener('click', closeGnbSearchHistoryOnClickingOutside)
   }
 }
 
@@ -34,7 +39,23 @@ window.addEventListener('click', closeGnbSearchHistoryOnClickingOutside)
 function deleteAllSearchHisoryItems() {
   //gnbSearchHistoryList 안에 있는 모든 li를 삭제!
   gnbSearchHistoryList.innerHTML = ''
-  gnbSearchHistory.classList.remove('is-active')
+  closeGnbSearchHistory()
 }
 
 deleteAllButton.addEventListener('click', deleteAllSearchHisoryItems)
+
+function deleteSearchHistoryItem(e) {
+  e.stopPropagation() //이벤트의 전파를 막아준다.
+  //li 삭제
+  const itemToDelete = this.parentNode
+  gnbSearchHistoryList.removeChild(itemToDelete)
+
+  //각각의 개별 리스트를 삭제 했을때 ol 창이 닫힘
+  if (gnbSearchHistoryList.children.length === 0) {
+    closeGnbSearchHistory()
+  }
+}
+
+deletButtonList.forEach((button) => {
+  button.addEventListener('click', deleteSearchHistoryItem)
+})
