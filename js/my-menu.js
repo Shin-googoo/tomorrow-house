45fa0230bd0cf73259d6c156db9b62ee6230ca9e
syg1702 <syg1702@gmail.com>
2024-03-16 12:13:24 +0900

    🚧 Implement toggling product-tab item

--- js/my-menu.js
+++ js/my-menu.js
@@ -11,7 +11,6 @@ function toggleMyMenu() {
 myMenuButton.addEventListener('click', toggleMyMenu)
 
 function closeMyMenuOnClickingOutside(e) {
-  console.log('window!!!')
   // 1. 내가 클릭한 요소가 무엇인지? -> e.target
   // 2. myMenu가 요소를 포함하고 있는지를 알 수 있는 방법 -> contains!
   // -> myMenu가 e.target을 포함하고 있지 않은 경우 -> myMenu에 is-active 제거
